2690914ce9e00c0a7c96e067cd68af7c3106e18f
Darciechapman <darciechapman96@gmail.com>
2020-04-22 11:01:17 +0800

    extending from empolyee

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+//const inquirer = require("inquirer");
+
+class Employee {
+    constructor(fullName, id, email) {
+        this.name = fullName;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Employee" ;
+    }
+}
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(githubName) {
+        this.Gitname = githubName;
+    }
+
+    getGithub() {
+        return this.Gitname;
+    }
+
+    getRole() {
+        return "Engineer" ;
+    }
+}
